c0d87789f0319bd7e3b7f4ffcc83221991a8351b
arnavpatil27 <1aaparnav@gmail.com>
2023-11-21 09:40:44 +0530

    new whatsapp like unterface

--- index.js
+++ index.js
@@ -3,10 +3,14 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
 const io = socketIo(server);
+
+const onlineUsers = new Set();
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -15,12 +19,28 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('a user connected');
 
+  socket.emit('requestUsername');
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
+    if (socket.username) {
+      onlineUsers.delete(socket.username);
+      io.emit('updateOnlineUsers', Array.from(onlineUsers));
+    }
+  });
+
+  socket.on('setUsername', (username) => {
+    socket.username = username;
+    onlineUsers.add(username);
+    io.to('chatroom').emit('chat message', `${username} has joined the chat.`);
+    io.emit('updateOnlineUsers', Array.from(onlineUsers));
   });
 
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    io.to('chatroom').emit('chat message', {
+      username: socket.username,
+      message: msg
+    });
   });
 });
 
